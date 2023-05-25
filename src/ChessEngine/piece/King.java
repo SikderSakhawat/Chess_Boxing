4fbcd32fe5ae0931caab52ebe026bc0af41043ab
SikderSakhawat <114007137+SikderSakhawat@users.noreply.github.com>
2023-05-24 23:31:27 -0400

    Fixed up the structure of the pawn class and the King class.

---
+++ src/ChessEngine/piece/King.java
@@ -0,0 +1,58 @@
+package ChessEngine.piece;
+
+import ChessEngine.Alliance;
+import ChessEngine.board.Board;
+import ChessEngine.board.BoardUtility;
+import ChessEngine.board.Move;
+import ChessEngine.board.Move.AttackMove;
+import ChessEngine.board.Move.MajorMove;
+import ChessEngine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class King extends Piece {
+
+    private static final int[] CANDIDATE_LEGAL_MOVES = {-9,-8,-7,-1,1,7,8,9};
+    public King(int piecePos, Alliance pieceAll) {
+        super(piecePos, pieceAll);
+    }
+
+    @Override
+    public Collection<Move> calcLegalMoves(Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+        for(final int currentCandidateOffset: CANDIDATE_LEGAL_MOVES){
+            final int candidateDestinationCoords = this.piecePosition + currentCandidateOffset;
+            if(isFirstColExclude(this.piecePosition, currentCandidateOffset) || isSecondColExclude(this.piecePosition, currentCandidateOffset)){
+                continue;
+            }
+            if(BoardUtility.isValidTileCoord(candidateDestinationCoords)){
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoords);
+
+            }
+            final Tile candidateDestinationTile = board.getTile(candidateDestinationCoords);
+            if(!candidateDestinationTile.isOccupied()){
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoords));
+            } else {
+                final Piece pieceDestination = candidateDestinationTile.getPiece();
+                final Alliance pieceAlliance = pieceDestination.getPiecedAlliance();
+
+                if(this.piecedAlliance != pieceAlliance){
+                    legalMoves.add(new AttackMove(board, this, candidateDestinationCoords, pieceDestination));
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    private static boolean isFirstColExclude(final int currentPos, final int candidateOffset){
+        return BoardUtility.FIRST_COLUMN[currentPos] && ((candidateOffset == -9) ||
+                (candidateOffset == -1) || (candidateOffset == 7));
+    }
+
+    private static boolean isSecondColExclude(final int currentPos, final int candidateOffset){
+        return BoardUtility.EIGHTH_COLUMN[currentPos] && ((candidateOffset == 1) || (candidateOffset == 9) || (candidateOffset == -7));
+    }
+}
--- src/ChessEngine/piece/Pawn.java
+++ src/ChessEngine/piece/Pawn.java
@@ -4,11 +4,12 @@ import ChessEngine.Alliance;
 import ChessEngine.board.Board;
 import ChessEngine.board.BoardUtility;
 import ChessEngine.board.Move;
+import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
+import static ChessEngine.board.Move.*;
 public class Pawn extends Piece{
     private static final int[] CANDIDATE_MOVE_COORD = {8,16, 7 ,9};
     public Pawn(final int piecePos, final Alliance pieceAll) {
@@ -18,7 +19,7 @@ public class Pawn extends Piece{
     @Override
     public Collection<Move> calcLegalMoves(Board board) {
 
-        final List<Move> legalmoves = new ArrayList<>();
+        final List<Move> legalMoves = new ArrayList<>();
         for(final int offset : CANDIDATE_MOVE_COORD){
             final int candidateDestinationCoord = this.piecePosition + (this.piecedAlliance.getDirection() * offset);
             if(BoardUtility.isValidTileCoord(candidateDestinationCoord)){
@@ -26,30 +27,42 @@ public class Pawn extends Piece{
             }
             // handles basic non-attacking pawn movement
             if(offset == 8 && !board.getTile(offset).isOccupied()){
-                // TODO more things here!
-                legalmoves.add(new Move.MajorMove(board, this, candidateDestinationCoord));
+                // TODO more things here (work on pawn promotion)!
+                legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoord));
             // handles the pawn jump from either 1 or 2 squares
             } else if(offset == 16 && this.isFirstMove() && (BoardUtility.SECOND_ROW[this.piecePosition]
                     && this.getPiecedAlliance().isBlack()) || (BoardUtility.SEVENTH_ROW[this.piecePosition]
                     && this.getPiecedAlliance().isWhite())){
                 final int behindDestinationCoord = this.piecePosition + (this.piecedAlliance.getDirection() * 8);
                 if(!board.getTile(behindDestinationCoord).isOccupied() && !board.getTile(candidateDestinationCoord).isOccupied()){
-                    legalmoves.add(new Move.MajorMove(board, this, candidateDestinationCoord));
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoord));
                 }
+                // case where we attack the piece to our right diagonal from us
             } else if (offset == 7 &&
-                   !(BoardUtility.EIGHTH_COLUMN[piecePosition] && this.piecedAlliance.isWhite()) ||
-                    (BoardUtility.FIRST_COLUMN[piecePosition] && this.piecedAlliance.isBlack())) {
+                   !((BoardUtility.EIGHTH_COLUMN[piecePosition] && this.piecedAlliance.isWhite() ||
+                    (BoardUtility.FIRST_COLUMN[piecePosition] && this.piecedAlliance.isBlack())))) {
                 if(board.getTile(candidateDestinationCoord).isOccupied()){
-
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoord).getPiece();
+                    if(this.piecedAlliance !=  pieceOnCandidate.getPiecedAlliance()){
+                        // TODO more (the case when we attack a piece and get into promotion)!
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoord));
+                    }
                 }
             } else if (offset == 9 &&
-                    (BoardUtility.EIGHTH_COLUMN[piecePosition] && this.piecedAlliance.isWhite()) ||
-                    (BoardUtility.FIRST_COLUMN[piecePosition] && this.piecedAlliance.isBlack())){
-
+                    !((BoardUtility.FIRST_COLUMN[piecePosition] && this.piecedAlliance.isWhite() ||
+                    (BoardUtility.EIGHTH_COLUMN[piecePosition] && this.piecedAlliance.isBlack())))){
+                if(board.getTile(candidateDestinationCoord).isOccupied()){
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoord).getPiece();
+                    if(this.piecedAlliance !=  pieceOnCandidate.getPiecedAlliance()){
+                        // TODO more (the case when we attack a piece and get into promotion)!
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoord));
+                    }
+                }
             }
         }
-        return null;
+        return ImmutableList.copyOf(legalMoves);
     }
 
+    // TODO Case when En Passant happens
 
 }
